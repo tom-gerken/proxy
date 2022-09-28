8149b0ca3dd3964340d30f1e9908158626cc4b0b
Tom Gerken <tgerken@gmail.com>
2022-09-28 13:39:43 -0700

    added heroku settings file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,32 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+
+  // Configuration
   const port = process.env.PORT || 3333;
+  const host = "localhost";
+  const api_service_url = "https://api.crexi.com";
+  const globalPrefix = '/api';
+  
+  app.setGlobalPrefix(globalPrefix);
+
+
+  // Proxy endpoints
+  app.use(globalPrefix, createProxyMiddleware({
+    target: api_service_url,
+    changeOrigin: true,
+    pathRewrite: {
+        [globalPrefix]: '',
+    }
+  }));
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
